f70bc979b33bbc10f918c12dc61614bfaf503d93
khal <khal.rahman@gmail.com>
2014-03-27 01:17:30 +0800

    init commits

---
+++ src/main/java/Java8er.java
@@ -0,0 +1,9 @@
+/**
+ * Created by nevda on 27/3/14.
+ */
+public class Java8er {
+    public static void main(String[] args){
+        Runnable r = () -> System.out.println("foo");
+        r.run();
+    }
+}
